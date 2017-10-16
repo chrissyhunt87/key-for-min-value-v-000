6ae7378ec815e0892d1f1b490e773e4fb0fa9d1f
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-16 22:15:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,7 @@
 def key_for_min_value(name_hash)
   smallest_key = nil
   name_hash.each do |key, value|
-    if smallest_key == nil
-      smallest_key = key
-    elsif value < name_hash[smallest_key]
+    if smallest_key == nil || value < name_hash[smallest_key]
       smallest_key = key
     else
       nil
